d15b325fd314befadb2e440d3cb3db973532720a
Kevin Cortéz <kcortezm@miumg.edu.gt>
2023-03-17 16:27:59 -0600

    Proyecto proceso

---
+++ Proyectoproceso.js
@@ -0,0 +1,3 @@
+function registrarse(){
+    location="login.html"
+}
---
+++ proyectoprocesoprincipal.js
@@ -0,0 +1,4 @@
+procesadores();{
+  var  lado2=parseInt(document.getElementById('lado2').value);
+    lado2=document.write(location('procesadores.txt'))
+}
