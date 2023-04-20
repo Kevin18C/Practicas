b5e562dfb33450bea3871465f88012165c73fb05
Kevin Cortéz <kcortezm@miumg.edu.gt>
2023-04-19 20:04:15 -0600

    Archivos formateados

--- loginprincipal.js
+++ loginprincipal.js
@@ -1,18 +1,17 @@
-const $btnSignIn= document.querySelector('.sign-in-btn'),
-      $btnSignUp = document.querySelector('.sign-up-btn'),  
-      $signUp = document.querySelector('.sign-up'),
-      $signIn  = document.querySelector('.sign-in');
+const $btnSignIn = document.querySelector(".sign-in-btn"),
+  $btnSignUp = document.querySelector(".sign-up-btn"),
+  $signUp = document.querySelector(".sign-up"),
+  $signIn = document.querySelector(".sign-in");
 
-document.addEventListener('click', e => {
-    if (e.target === $btnSignIn || e.target === $btnSignUp) {
-        $signIn.classList.toggle('active');
-        $signUp.classList.toggle('active')
-    }
+document.addEventListener("click", (e) => {
+  if (e.target === $btnSignIn || e.target === $btnSignUp) {
+    $signIn.classList.toggle("active");
+    $signUp.classList.toggle("active");
+  }
 });
 
-
-const registerForm = document.getElementById('registerForm');
-const loginForm = document.getElementById('loginForm');
+const registerForm = document.getElementById("registerForm");
+const loginForm = document.getElementById("loginForm");
 
 // Función para registrar un nuevo usuario
 function registerUser(event) {
@@ -28,12 +27,12 @@ function registerUser(event) {
   }
 
   if (isEmailUsed(email)) {
-    alert('Este correo electrónico ya ha sido utilizado!');
+    alert("Este correo electrónico ya ha sido utilizado!");
   } else {
     // Guarda los valores de usuario y contraseña en un objeto y lo guarda en el almacenamiento local del navegador
     const user = { email, password };
     localStorage.setItem(email, JSON.stringify(user));
-    alert('Usuario registrado exitosamente!');
+    alert("Usuario registrado exitosamente!");
     location.href = "Paginaprincipal.html";
   }
 }
@@ -51,25 +50,18 @@ function loginUser(event) {
   if (savedUserJSON !== null) {
     const savedUser = JSON.parse(savedUserJSON);
     if (savedUser.password === password) {
-      alert('Inicio de sesión exitoso!');
+      alert("Inicio de sesión exitoso!");
       location.href = "Paginaprincipal.html";
     } else {
-      alert('Nombre de usuario o contraseña incorrectos!');
+      alert("Nombre de usuario o contraseña incorrectos!");
     }
   } else {
-    alert('Nombre de usuario o contraseña incorrectos!');
+    alert("Nombre de usuario o contraseña incorrectos!");
   }
 }
 
 // Agrega un controlador de eventos para el envío del formulario de registro
-registerForm.addEventListener('submit', registerUser);
+registerForm.addEventListener("submit", registerUser);
 
 // Agrega un controlador de eventos para el envío del formulario de inicio de sesión
-loginForm.addEventListener('submit', loginUser);
-
-
-
-
-
-
-
+loginForm.addEventListener("submit", loginUser);
